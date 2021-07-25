b3d5d1b2739c94634113ad67f349743202d0accd
MullaiArgent <mullairajan2000@gmail.com>
2021-07-26 00:45:03 +0530

    Data Access Object Design for Fetching and Updateing the Data.

--- Data Definition Language/DDL.java
+++ Data Definition Language/DDL.java
@@ -8,7 +8,7 @@ class DDL {
 		
 		String url = "jdbc:mysql://localhost:3306/testrun";
 		String uname = "root";
-		String pass = "*** replace with ur Password ***";
+		String pass = "*** replace with urs****";
 		Connection con = DriverManager.getConnection(url,uname,pass);
 		
 		String query = "select * from student";
